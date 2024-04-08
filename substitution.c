c386aff787ef386c577236fd0153eae56c59aba7
Ewaldouhlmann <Ewaldouhlmann@users.noreply.github.com>
2024-04-08 12:58:43 -0300

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -15,8 +15,7 @@ int main(int argc, string argv[])
         if (strlen(argv[1]) == 26 && !is_validEntry)
         {
             string plaintext = get_string("Plaintext: ");
-            string upper_cipher = toupper(argv[1])
-            char* ciphertext = get_cipher(plaintext, upper_cipher);
+            char* ciphertext = get_cipher(plaintext, argv[1]);
             printf("ciphertext: %s\n", ciphertext);
             free(ciphertext);
         }
